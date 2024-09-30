631be13d99970998c3486ee9946b04c3e61e2a0f
DonatoAletta <donatoalettawork@gmail.com>
2024-09-30 16:01:37 +0200

    Initializer

---
+++ src/test/java/com/develhope/Java27_progetto3_team2/Java27Progetto3Team2ApplicationTests.java
@@ -0,0 +1,16 @@
+/*package com.develhope.Java27_progetto3_team2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java27Progetto3Team2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+
+
+ */
