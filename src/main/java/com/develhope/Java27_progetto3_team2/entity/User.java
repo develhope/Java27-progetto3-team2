fa70a24cd0ede7ab2cf3210bdb57832074a23cbc
DonatoAletta <donatoalettawork@gmail.com>
2024-10-03 21:10:35 +0200

    Added Entity and Column names. Added Generated Value to id

--- src/main/java/com/develhope/Java27_progetto3_team2/entity/User.java
+++ src/main/java/com/develhope/Java27_progetto3_team2/entity/User.java
@@ -1,22 +1,24 @@
 package com.develhope.Java27_progetto3_team2.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Entity
+@Entity(name = "user")
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
 public class User {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Setter
+    @Column(name = "name")
     private String name;
     @Setter
+    @Column(name = "surname")
     private String surname;
 }
