a1122ed99ce68bff68ee7db0c63a50ad042c264b
BuildTools <unconfigured@null.spigotmc.org>
2024-10-10 15:19:06 +0200

    fix/removed if in the addNewOrder method of the class OrderService

--- src/main/java/com/develhope/Java27_progetto3_team2/service/OrderService.java
+++ src/main/java/com/develhope/Java27_progetto3_team2/service/OrderService.java
@@ -14,9 +14,6 @@ public class OrderService {
     private final OrderRepository orderRepository;
 
     public OrderDTO addNewOrder(OrderDTO orderDTO) throws Exception {
-        if(orderRepository.existsById(orderDTO.getId())){
-            throw new Exception("order exists!");
-        }
         Order order = orderMapper.mapperOrderDTOToOrder(orderDTO);
         orderRepository.save(order);
         return orderMapper.mapperOrderToOrderDTO(order);
