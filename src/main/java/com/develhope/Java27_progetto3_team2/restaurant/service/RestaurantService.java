abc636243ae62d8812b5d9b95c3d1aa05bc2dcc3
DonatoAletta <donatoalettawork@gmail.com>
2024-10-17 11:36:00 +0200

    Added RestaurantService

---
+++ src/main/java/com/develhope/Java27_progetto3_team2/restaurant/service/RestaurantService.java
@@ -0,0 +1,42 @@
+package com.develhope.Java27_progetto3_team2.restaurant.service;
+
+import com.develhope.Java27_progetto3_team2.restaurant.dto.RestaurantDTO;
+import com.develhope.Java27_progetto3_team2.restaurant.model.Restaurant;
+import com.develhope.Java27_progetto3_team2.restaurant.repository.RestaurantRepository;
+import com.develhope.Java27_progetto3_team2.restaurant.utils.RestaurantMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class RestaurantService {
+
+    private final RestaurantRepository restaurantRepository;
+    private final RestaurantMapper restaurantMapper;
+
+    public Page<RestaurantDTO> getAllRestaurants(int page, int quantity){
+        Pageable pageable = PageRequest.of(page,quantity);
+        Page<Restaurant> restaurantsList = restaurantRepository.findAllPaginated(pageable);
+        return restaurantsList.map(restaurantMapper::toDTO);
+    }
+
+    public RestaurantDTO getRestaurantById(Long id) throws Exception {
+        Restaurant restaurant = restaurantRepository
+                .findById(id)
+                .orElseThrow(() -> new Exception("Restaurant with id: " + id + " not found!"));
+        return restaurantMapper.toDTO(restaurant);
+    }
+
+    public List<RestaurantDTO> getRestaurantByCategory(String category){
+        List<Restaurant> restaurants = restaurantRepository.findByCategory(category);
+        return restaurants
+                .stream()
+                .map(restaurantMapper::toDTO).toList();
+    }
+
+}
