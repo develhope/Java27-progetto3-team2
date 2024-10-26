392ad9928e58b339670e20d37bc460ea0eec4c5b
DonatoAletta <donatoalettawork@gmail.com>
2024-10-26 12:45:14 +0200

    Added findByEmail

--- src/main/java/com/develhope/Java27_progetto3_team2/user/UserRepository.java
+++ src/main/java/com/develhope/Java27_progetto3_team2/user/UserRepository.java
@@ -3,7 +3,10 @@ package com.develhope.Java27_progetto3_team2.user;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository <User,Long> {
+    Optional<User> findByEmail(String email);
 
 }
