407455843ff7a5cc303ec653f03f9351d9da4904
Fabio R <fabioraffa01@gmail.com>
2024-10-27 17:07:36 +0100

    - Added changes from origin

--- src/main/java/com/develhope/Java27_progetto3_team2/menu/model/dto/MenuItemDTO.java
+++ src/main/java/com/develhope/Java27_progetto3_team2/menu/model/dto/MenuItemDTO.java
@@ -2,27 +2,21 @@ package com.develhope.Java27_progetto3_team2.menu.model.dto;
 
 import lombok.*;
 
+@Setter
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+
 public class MenuItemDTO {
     private Long id;
-    @Setter
     private String itemName;
-    @Setter
     private String itemDescription;
-    @Setter
     private double itemPrice;
-    @Setter
     private String categoryFood;
-    @Setter
     private boolean availability;
-    @Setter
     private boolean vegetarianItem;
-    @Setter
     private boolean glutenFreeItem;
-    @Setter
     private boolean lactoseFreeItem;
     private Long menuId;
 }
