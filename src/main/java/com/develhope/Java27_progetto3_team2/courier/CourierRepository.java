27c8fb7c1bdcc206501188bfd1d927adfb116eda
Maria Eduarda Barone <127447739+dudinhab@users.noreply.github.com>
2024-10-30 10:21:59 +0100

    Update CourierRepository.java

--- src/main/java/com/develhope/Java27_progetto3_team2/courier/CourierRepository.java
+++ src/main/java/com/develhope/Java27_progetto3_team2/courier/CourierRepository.java
@@ -2,6 +2,7 @@ package com.develhope.Java27_progetto3_team2.courier;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+@Repository
 public interface CourierRepository extends JpaRepository<Courier, Long> {
     Courier findByEmail(String email);
 }
