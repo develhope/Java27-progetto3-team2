b442ca9c221c5390e08bb404da298108caebbde0
DonatoAletta <donatoalettawork@gmail.com>
2024-10-31 15:01:04 +0100

    Added CourierAlreadyExistException

---
+++ src/main/java/com/develhope/Java27_progetto3_team2/exception/CourierAlreadyExistException.java
@@ -0,0 +1,7 @@
+package com.develhope.Java27_progetto3_team2.exception;
+
+public class CourierAlreadyExistException extends RuntimeException{
+    public CourierAlreadyExistException(String message){
+        super(message);
+    }
+}
