678f511f66bb922e07fc084a30a9e68ad094653a
DonatoAletta <donatoalettawork@gmail.com>
2024-10-31 15:01:11 +0100

    Added CreateCourierDTO

---
+++ src/main/java/com/develhope/Java27_progetto3_team2/courier/CreateCourierDTO.java
@@ -0,0 +1,14 @@
+package com.develhope.Java27_progetto3_team2.courier;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@NoArgsConstructor
+@SuperBuilder
+public class CreateCourierDTO extends CourierDTO{
+
+}
