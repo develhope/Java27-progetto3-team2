d9bf0a43633c9e4f22182d367b5ae0f9e13c2fd3
DonatoAletta <donatoalettawork@gmail.com>
2024-11-03 12:20:22 +0100

    removed the order mapping

--- src/main/java/com/develhope/Java27_progetto3_team2/cart/mapper/CartMapper.java
+++ src/main/java/com/develhope/Java27_progetto3_team2/cart/mapper/CartMapper.java
@@ -2,13 +2,11 @@ package com.develhope.Java27_progetto3_team2.cart.mapper;
 
 import com.develhope.Java27_progetto3_team2.cart.cart.model.Cart;
 import com.develhope.Java27_progetto3_team2.cart.cart.model.CartDTO;
-import com.develhope.Java27_progetto3_team2.cart.service.CartService;
 import com.develhope.Java27_progetto3_team2.cart.service.CartServiceMapper;
 import com.develhope.Java27_progetto3_team2.order.OrderService;
 import com.develhope.Java27_progetto3_team2.restaurant.service.RestaurantService;
 import com.develhope.Java27_progetto3_team2.user.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -20,9 +18,9 @@ public class CartMapper {
     private final UserService userService;
 
     public CartDTO cartToCartDTO(Cart cart){
-        return CartDTO.builder().id(cart.getId()).cartItemIdList(cartService.getCartItemListIdfromCartItemList(cart.getCartItemList())).restaurantId(cart.getRestaurant().getId()).status(cart.getStatus()).orderId(cart.getOrder().getId()).userId(cart.getUser().getId()).build();
+        return CartDTO.builder().id(cart.getId()).cartItemIdList(cartService.getCartItemListIdfromCartItemList(cart.getCartItemList())).restaurantId(cart.getRestaurant().getId()).status(cart.getStatus()).userId(cart.getUser().getId()).build();
     }
     public Cart cartDTOToCart(CartDTO cartDTO){
-        return Cart.builder().id(cartDTO.getId()).cartItemList(cartService.getCartItemListfromCartItemIdList(cartDTO.getCartItemIdList())).restaurant(restaurantService.getRestaurantById(cartDTO.getRestaurantId())).status(cartDTO.getStatus()).order(orderService.getOrderById(cartDTO.getOrderId())).user(userService.getUserById(cartDTO.getUserId())).build();
+        return Cart.builder().id(cartDTO.getId()).cartItemList(cartService.getCartItemListfromCartItemIdList(cartDTO.getCartItemIdList())).restaurant(restaurantService.getRestaurantById(cartDTO.getRestaurantId())).status(cartDTO.getStatus()).user(userService.getUserById(cartDTO.getUserId())).build();
     }
 }
