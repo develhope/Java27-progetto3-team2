c0a7254a8ca425bab352ca4b2e49afe3333ea928
DonatoAletta <donatoalettawork@gmail.com>
2024-11-10 14:49:01 +0100

    added DeliveryTime and arrivint time to UserOrderDTO

--- src/main/java/com/develhope/Java27_progetto3_team2/order/dto/UserOrderDTO.java
+++ src/main/java/com/develhope/Java27_progetto3_team2/order/dto/UserOrderDTO.java
@@ -1,6 +1,7 @@
 package com.develhope.Java27_progetto3_team2.order.dto;
 
 import com.develhope.Java27_progetto3_team2.cart.cart.model.CartDTO;
+import com.develhope.Java27_progetto3_team2.order.enumerator.OrderStatus;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -16,8 +17,10 @@ public class UserOrderDTO {
     private String deliveryAddress;
     private String paymentMethod;
     private LocalDateTime orderDate;
+    private LocalDateTime deliveryTime;
+    private String arrivingTime;
     private Double totalPrice;
     private String restaurantName;
     private CartDTO cartDTO;
-    private LocalDateTime deliveryTime;
+    private OrderStatus orderStatus = OrderStatus.DELIVERING;
 }
