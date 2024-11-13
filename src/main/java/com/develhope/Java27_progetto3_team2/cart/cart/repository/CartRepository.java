a7e1cbe7cbcb8b2c584a5cbc41beedf88e704ea9
DonatoAletta <donatoalettawork@gmail.com>
2024-11-13 21:11:17 +0100

    Changed the return type offindByUser_Id to Optional

--- src/main/java/com/develhope/Java27_progetto3_team2/cart/cart/repository/CartRepository.java
+++ src/main/java/com/develhope/Java27_progetto3_team2/cart/cart/repository/CartRepository.java
@@ -4,7 +4,9 @@ import com.develhope.Java27_progetto3_team2.cart.cart.model.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CartRepository extends JpaRepository<Cart, Long> {
-    Cart findByUser_Id(Long id);
+    Optional<Cart> findByUser_Id(Long id);
 }
